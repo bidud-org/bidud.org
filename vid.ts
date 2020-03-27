1b287718af4e15f08c699351c5ae74e48c621af7
Gilad Kutiel <gilad.kutiel@gmail.com>
2020-03-27 19:01:55 +0300

    refactor

--- vid.ts
+++ vid.ts
@@ -5,11 +5,19 @@ export namespace data {
 
     export const vids = [
         {
-            src: 'vids/03_02.mp4',
+            src: 'vids/01-02.mp4',
             cues: [
-                { s: 0, e: 2, t: '' },
-                { s: 5, e: 9, t: '' },
+                { s: 3, e: 5, t: '' },
+                { s: 8, e: 12, t: '' },
             ]
-        }
+        },
+        {
+            src: 'vids/01-03.mp4',
+            cues: [
+                { s: 2, e: 4, t: '' },
+                { s: 10, e: 16, t: '' },
+                { s: 19, e: 23, t: '' },
+            ]
+        },
     ]
 }
