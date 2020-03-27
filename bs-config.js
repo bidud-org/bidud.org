6f07cc14507cd067297697d87fb52ad9610a9013
Gilad Kutiel <gilad.kutiel@gmail.com>
2020-03-27 11:06:17 +0300

    npm

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    files: [
+        "docs/*.css",
+        "docs/*.js",
+        "docs/*.html",
+        "src/*.js",
+    ],
+    proxy: 'localhost:8979'
+};
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,28 @@ module.exports = {
         filename: '[name].b.js',
         path: __dirname + '/docs'
     },
-    mode: 'development'
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
 };
