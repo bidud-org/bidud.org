0e9124371c556e329c2b42e2ee55c3ff6192893c
Gilad Kutiel <gilad.kutiel@gmail.com>
2020-03-27 18:51:32 +0300

    refactor

--- vid.ts
+++ vid.ts
@@ -1,61 +1,14 @@
 
-export namespace $ {
+export namespace data {
     export type cue = { s: number, e: number, t: string }
     export type vid = { src: string, cues: cue[] }
 
-    const vids = [
+    export const vids = [
         {
-            src: 'vid_01.mp4',
+            src: 'vids/03_02.mp4',
             cues: [
-                { s: 1, e: 3, t: '' },
-                { s: 4, e: 6, t: '' },
-                { s: 7, e: 11, t: '' },
-                { s: 14, e: 16, t: '' },
-                { s: 17, e: 22, t: '' },
-                { s: 24, e: 29, t: '' },
-            ]
-        }
-        , {
-            src: 'vid_02.mp4',
-            cues: [
-                { s: 1, e: 3, t: '' },
-                { s: 3.5, e: 7, t: '' },
-                { s: 8, e: 14, t: '' },
-                { s: 15, e: 23, t: '' },
-                { s: 25, e: 28, t: '' },
-                { s: 30, e: 31, t: '' },
-                { s: 38, e: 40, t: '' },
-            ]
-        }
-        , {
-            src: 'vid_03.mp4',
-            cues: [
-                { s: 1, e: 2, t: '' },
-                { s: 3, e: 4, t: '' },
-                { s: 5, e: 10, t: '' },
-                { s: 12, e: 15, t: '' },
-                { s: 16, e: 22, t: '' },
-            ]
-        }
-        , {
-            src: 'vid_04.mp4',
-            cues: [
-                { s: 2, e: 5, t: '' },
-                { s: 5, e: 8, t: '' },
-                { s: 8, e: 10, t: '' },
-                { s: 12, e: 14, t: '' },
-            ]
-        }
-        , {
-            src: 'vid_05.mp4',
-            cues: [
-                { s: 1, e: 4, t: '' },
-                { s: 6, e: 9, t: '' },
-                { s: 10, e: 11, t: '' },
-                { s: 12, e: 15, t: '' },
-                { s: 16, e: 19, t: '' },
-                { s: 20, e: 24, t: '' },
-                { s: 24, e: 26, t: '' },
+                { s: 0, e: 2, t: '' },
+                { s: 5, e: 9, t: '' },
             ]
         }
     ]
--- view.ts
+++ view.ts
@@ -1,14 +1,6 @@
 import * as queryString from 'query-string'
 import './view.css'
-import * as Tokenizer from 'sentence-tokenizer'
-import sbd from 'sbd'
-
-// function f2t(seconds: number) {
-//     const min = Math.floor(seconds / 60)
-//     const sec = (seconds % 60).toFixed(3)
-
-//     return ('00' + min).slice(-2) + ':' + ('00' + sec).slice(-6)
-// }
+import { data } from './vid'
 
 // export function rndInt() {
 //     return Math.floor(Math.random() * vids.length);
@@ -18,14 +10,6 @@ import sbd from 'sbd'
 //     return vids[i]
 // }
 
-
-// export function vtt(cues: cue[]) {
-//     const sub = 'WEBVTT\n\n' + cues.map(
-//         c => `${f2t(c.s)} --> ${f2t(c.e)}\n${c.t}`
-//     ).join('\n\n')
-
-//     return URL.createObjectURL(new Blob([sub], { type: 'text/vtt' }))
-// }
 
 // export function debounce<F extends (...args: any[]) => void>(
 //     f: F
@@ -39,28 +23,63 @@ import sbd from 'sbd'
 //     }) as any
 // }
 
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
 
 function sents(txt) {
     return txt.split('\n')
 }
 
+function getVid(sentences: string[]) {
+    const vids = shuffle(data.vids)
+
+
+    for (const vid of vids) {
+        if (vid.cues.length === sentences.length)
+            return vid
+    }
+}
+
+function f2t(seconds: number) {
+    const min = Math.floor(seconds / 60)
+    const sec = (seconds % 60).toFixed(3)
+
+    return ('00' + min).slice(-2) + ':' + ('00' + sec).slice(-6)
+}
+
+function vtt(cues: data.cue[]) {
+    const sub = 'WEBVTT\n\n' + cues.map(
+        c => `${f2t(c.s)} --> ${f2t(c.e)}\n${c.t}`
+    ).join('\n\n')
+
+    console.log(sub)
+    return URL.createObjectURL(new Blob([sub], { type: 'text/vtt' }))
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     const args = queryString.parse(location.search)
     const txt = args.txt as string
-
-    // const t = new Tokenizer()
     const sentences = sents(txt)
-    console.log(sentences)
+    const vid = getVid(sentences)
 
-    // const player = document.getElementById('player') as HTMLMediaElement
-    // player.src = vid.src
+    for (let i in sentences) {
+        vid.cues[i].t = sentences[i]
+    }
 
-    // const track = document.createElement('track')
-    // track.kind = 'subtitles'
-    // track.srclang = 'en'
-    // track.src = $.vtt(cues)
-    // player.appendChild(track)
-    // player.textTracks[0].mode = 'showing'
+    const player = document.getElementById('player') as HTMLMediaElement
+    player.src = vid.src
 
-    // player.play()
+    const track = document.createElement('track')
+    track.kind = 'subtitles'
+    track.srclang = 'en'
+    track.src = vtt(vid.cues)
+    player.appendChild(track)
+    player.textTracks[0].mode = 'showing'
+
+    player.play()
 })
